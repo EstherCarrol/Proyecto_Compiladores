aad52d7884cbd506f360db3c88c5f4e1dcfd8e79
nikolas-oss <nicolastislovomontenegro@gmail.com>
2023-08-18 04:52:11 -0600

    Agregando Resultado Aleatorio

--- App.py
+++ App.py
@@ -1,10 +1,13 @@
-import tkinter as tk
+#Librerias utilizadas
 import re
+import random
 from tkinter import ttk
+import tkinter as tk
 from tkinter import filedialog
 from tkinter import Toplevel
 from tkinter import scrolledtext 
-
+import random
+from Proyecto_compiladores import obtenerOperaciones
 from Proyecto_compiladores import tabla_lexica
 from Proyecto_compiladores import resultados_conversion,resultado_arreglo_operaciones,num_count, roman_count, oct_count, hex_count, bin_count, maya_count, domino_count
 from Proyecto_compiladores import convertir
@@ -16,6 +19,7 @@ tabla = [
     ["ROMANO", "Romano", 2],
     ["BINARIO", "Binario", 3]
 ]
+#Convertir numero de tokens en porcentaje 
 
 total_tokens = num_count + roman_count + oct_count + hex_count + bin_count + maya_count + domino_count
 
@@ -29,6 +33,8 @@ porcentaje_distribucion = {
     "DOMINO": (domino_count / total_tokens) * 100,
 }
 
+#Funcion para la tabla de analisis lexico
+
 def mostrar_lexico(data):
     ventana_lexico = Toplevel(root)
     ventana_lexico.title("Tabla de Análisis Léxico")
@@ -36,11 +42,14 @@ def mostrar_lexico(data):
     frame = tk.Frame(ventana_lexico)
     frame.pack(fill="both", expand=True)
 
-    tabla = ttk.Treeview(frame, show="headings")
+    style = ttk.Style()
+    style.configure("Treeview", font=("Arial", 15))
+    
+    tabla = ttk.Treeview(frame, show="headings", height=20, style="Treeview")
     tabla['columns'] = data[0]
     for col in data[0]:
         tabla.heading(col, text=col)
-        tabla.column(col, anchor='center', width=300)
+        tabla.column(col, anchor='center', width=200)
 
     # Insertar filas
     for row in data[1:]:
@@ -52,14 +61,17 @@ def mostrar_lexico(data):
     scrollbar.pack(side="right", fill="y")
     tabla.config(yscrollcommand=scrollbar.set)
 
-    ventana_lexico.geometry("+100+100") 
+    ventana_lexico.geometry("900x500")  # Ajustar el tamaño de la ventana
+    ventana_lexico.resizable(False, False)
 
+#Desarrolladores
 
 def mostrar_about():
     limpiar_canvas()
     
     parrafo = """
-        Integrantes:
+        Integrantes Grupo #1 IS-913:
+        
         - Gelen Fabiola Amador Pavón 
         - Gleny Gissela Nihimaya Torres
         - Jennebier Esther Alvarado López
@@ -67,7 +79,7 @@ def mostrar_about():
         - Michael David Chang Oseguera
         - Nicolás Antonio Lovo Montenegro
     """
-    canvas.create_text(10, 10, text=parrafo, anchor="nw", fill="black", font=("Arial", 12))
+    canvas.create_text(10, 10, text=parrafo, anchor="nw",fill="black", font=("Arial", 15))
 
 def mostrar_archivo():
     global current_file
@@ -80,8 +92,13 @@ def mostrar_archivo():
             canvas.delete("all")
             canvas.create_text(10, 10, text=contenido, anchor="nw", fill="black", font=("Arial", 12))
         
+    
+
+    
+ #funcion para edicion de archivo   
 
 def editar_archivo():
+  
     ruta_archivo = filedialog.askopenfilename(filetypes=[("Archivos de Texto", "*.txt")])
     if ruta_archivo:
         with open(ruta_archivo, 'r') as archivo:
@@ -92,7 +109,6 @@ def editar_archivo():
             editor = scrolledtext.ScrolledText(ventana_editor, wrap=tk.WORD)
             editor.insert('1.0', contenido)
             editor.pack(fill=tk.BOTH, expand=True)
-            
             editor.focus_set()  # Desactivar el enfoque automático en el scrolledtext
 
             def guardar():
@@ -101,20 +117,29 @@ def editar_archivo():
                     archivo.write(nuevo_contenido)
                 ventana_editor.destroy()
 
+                # Actualizar resultados después de guardar
+                global resultado_arreglo_operaciones, resultados_conversion
+                resultado_arreglo_operaciones = obtenerOperaciones(nuevo_contenido)
+                resultados_conversion = realizarConversiones(resultado_arreglo_operaciones)
+                
+                limpiar_canvas()
+                canvas.delete("all")
+                canvas.create_text(10, 10, text=nuevo_contenido, anchor="nw", fill="black", font=("Arial", 14))
+
+
             boton_guardar = tk.Button(ventana_editor, text="Guardar", command=guardar)
             boton_guardar.pack(pady=10)
             
-            ventana_alto = ventana_editor.winfo_reqheight()
-
-            posicion_y = int((ventana_editor.winfo_screenheight() / 2) - (ventana_alto / 2))
-    
-            ventana_editor.geometry(f"{posicion_y}")
-
+            ventana_editor.resizable(False,False)
+            ventana_editor.geometry("600x500") 
 
 def limpiar_canvas():
     canvas.delete("all")
     current_file = None
     
+    
+#funcion para desplegar el modal de informacion secundaria
+
 def mostrar_informacion_secundaria():
     info_window = Toplevel(root)
     info_window.title("INFORMACION SECUNDARIA")
@@ -122,74 +147,92 @@ def mostrar_informacion_secundaria():
     info_frame = ttk.Frame(info_window)
     info_frame.pack(fill="both", expand=True)
 
-    info_label = tk.Label(info_frame, text="INFORMACION SECUNDARIA:")
-    info_label.pack()
-
-    info_text = scrolledtext.ScrolledText(info_frame, wrap=tk.WORD)
+    info_text = scrolledtext.ScrolledText(info_frame, wrap=tk.WORD, font=("Arial", 14))
     info_text.pack(fill=tk.BOTH, expand=True)
 
-    info_text.insert(tk.END, f"El token NUMERO aparece {num_count} veces en el archivo de entrada\n")
-    info_text.insert(tk.END, f"El token ROMANO aparece {roman_count} veces en el archivo de entrada\n")
-    info_text.insert(tk.END, f"El token OCTAL aparece {oct_count} veces en el archivo de entrada\n")
-    info_text.insert(tk.END, f"El token HEXADECIMAL aparece {hex_count} veces en el archivo de entrada\n")
-    info_text.insert(tk.END, f"El token BINARIO aparece {bin_count} veces en el archivo de entrada\n")
-    info_text.insert(tk.END, f"El token MAYA aparece {maya_count} veces en el archivo de entrada\n")
-    info_text.insert(tk.END, f"El token DOMINO aparece {domino_count} veces en el archivo de entrada\n")
+    info_text.tag_configure("center", justify="center")
+    info_text.insert(tk.END, f"El token NUMERO aparece {num_count} veces en el archivo de entrada\n", "center")
+    info_text.insert(tk.END, f"El token ROMANO aparece {roman_count} veces en el archivo de entrada\n", "center")
+    info_text.insert(tk.END, f"El token OCTAL aparece {oct_count} veces en el archivo de entrada\n", "center")
+    info_text.insert(tk.END, f"El token HEXADECIMAL aparece {hex_count} veces en el archivo de entrada\n", "center")
+    info_text.insert(tk.END, f"El token BINARIO aparece {bin_count} veces en el archivo de entrada\n", "center")
+    info_text.insert(tk.END, f"El token MAYA aparece {maya_count} veces en el archivo de entrada\n", "center")
+    info_text.insert(tk.END, f"El token DOMINO aparece {domino_count} veces en el archivo de entrada\n", "center")
 
-    info_text.insert(tk.END, "\nDistribución de porcentajes:\n")
+    info_text.insert(tk.END, "\nDistribución de porcentajes:\n", "center")
     for token_type, porcentaje in porcentaje_distribucion.items():
-        info_text.insert(tk.END, f"Porcentaje de {token_type}: {porcentaje:.2f}%\n")
+        info_text.insert(tk.END, f"Porcentaje de {token_type}: {porcentaje:.2f}%\n", "center")
 
-    info_text.config(state=tk.DISABLED)  # Hace que el texto no sea editable
+    info_text.config(state=tk.DISABLED)  
 
     cerrar_button = tk.Button(info_frame, text="Cerrar", command=info_window.destroy)
     cerrar_button.pack(pady=10)
 
-    info_window.geometry("+100+100") 
-    
+    info_window.geometry("600x400") 
+    info_window.resizable(False, False)
+
+
 def realizarConversiones(arreglo):
-    resultados = []
+    resultados=[]
     for elemento in arreglo:
         match = re.match(r'(\d+)(\w+)', elemento)
         if match:
             numero = match.group(1)
             conversion = match.group(2)
-            resultado = convertir(numero, conversion)
-            if resultado is not None:  # Agregar esta verificación
-                resultados.append(resultado)
+            if conversion == "Aleatorio":
+                resultado = convertir_aleatoriamente(numero)
             else:
-                resultados.append("Conversión no válida")
+                resultado = convertir(numero, conversion)
         else:
-            resultados.append("Operación desconocida")
+            resultado="Operación desconocida"
+        resultados.append(resultado)
     return resultados
 
+def convertir_aleatoriamente(numero):
+    sistemas_conversion = ["Romano", "Binario", "Octal", "Hexadecimal", "Maya", "Domino"]
+    sistema_aleatorio = random.choice(sistemas_conversion)
+    resultado = convertir(numero, sistema_aleatorio)
+    return resultado
+
     
+
 def mostrar_resultados_modal():
     resultados_window = Toplevel(root)
-    resultados_window.title("Resultados de Conversión")
 
-    resultados_text = scrolledtext.ScrolledText(resultados_window, wrap=tk.WORD)
+
+    resultados_frame = ttk.Frame(resultados_window)
+    resultados_frame.pack(fill="both", expand=True)
+
+    resultados_label = tk.Label(resultados_frame, text="RESULTADOS DE CONVERSION", font=("Arial", 20, "bold"))
+    resultados_label.pack()
+
+    resultados_text = scrolledtext.ScrolledText(resultados_frame, wrap=tk.WORD, font=("Arial", 30))
     resultados_text.pack(fill=tk.BOTH, expand=True)
 
+    resultados_text.tag_configure("center", justify="center")
+    
+    # Actualizar resultados antes de mostrarlos
+    resultados_conversion = realizarConversiones(resultado_arreglo_operaciones)
+
     for conversion, resultado in zip(resultado_arreglo_operaciones, resultados_conversion):
-        resultados_text.insert(tk.END, f"{conversion}: {resultado} \n ")
- 
-    resultados_text.config(state=tk.DISABLED)  # Hace que el texto no sea editable
+        resultados_text.insert(tk.END, f"{conversion}------>{resultado} \n", "center")
 
-    cerrar_button = tk.Button(resultados_window, text="Cerrar", command=resultados_window.destroy)
-    cerrar_button.pack(pady=10)
+    resultados_text.config(state=tk.DISABLED) 
 
-    resultados_window.geometry("+100+100") 
+    resultados_window.geometry("700x500")  
+    resultados_window.resizable(False, False)
     
 # Crear la ventana principal
+
 root = tk.Tk()
 root.title("Convertidor de números")
+
 
 icon_path = "ico/icon.png"
 icon = tk.PhotoImage(file=icon_path)
 icon_resized = icon.subsample(20)  # Adjust the subsample factor for desired size
 
-# Set the resized icon as the window's icon
+# Colocar icono
 root.iconphoto(True, icon_resized)
 
 # Crear botones
@@ -199,19 +242,20 @@ button_frame.pack(side="top", fill="x")
 
 lexico_button = tk.Button(button_frame, text="Léxico", command=lambda: mostrar_lexico(tabla_lexica))
 sintactico_button = tk.Button(button_frame, text="Desarrolladores", command=mostrar_about)
-archivo_button = tk.Button(button_frame, text="Archivo", command=mostrar_archivo)
 boton_informacion = tk.Button(button_frame, text="Información Secundaria", command=mostrar_informacion_secundaria)
-boton_editar = tk.Button(button_frame, text="Editar Archivo", command=editar_archivo)
+boton_editar = tk.Button(button_frame, text="Archivo", command=editar_archivo)
 boton_mostrar_resultados = tk.Button(button_frame, text="Mostrar Resultados", command=mostrar_resultados_modal)
 
+#Posiciones matriciales de los botones en la ventana principal
 
-archivo_button.pack(side="left", padx=5, pady=5)
 boton_editar.pack(side="left", padx=5, pady=5)
 lexico_button.pack(side="left", padx=5, pady=5)
 boton_informacion.pack(side="left", padx=5, pady=5)
 boton_mostrar_resultados.pack(side="left", padx=5, pady=5)
 sintactico_button.pack(side="left", padx=5, pady=5)
+
 # Crear canvas con scrollbar
+
 canvas_frame = tk.Frame(root)
 canvas_frame.pack(side="left", fill="both", expand=True)
 
@@ -221,5 +265,10 @@ canvas_scrollbar.pack(side="right", fill="y")
 canvas = tk.Canvas(canvas_frame, yscrollcommand=canvas_scrollbar.set)
 canvas.pack(side="left", fill="both", expand=True)
 canvas_scrollbar.config(command=canvas.yview)
+
+
+root.geometry("+400+150") 
+root.resizable(False, False)
+
 
 root.mainloop()
